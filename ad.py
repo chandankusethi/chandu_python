4d27e790c5e333c7c4e1e3ab230e560026751fd0
chandankusethi <162361083+chandankusethi@users.noreply.github.com>
2024-03-12 22:13:07 +0530

    Update ad.py

--- ad.py
+++ ad.py
@@ -1,27 +1,27 @@
-# a=20
-# b=10
-# x=a+b
-# print(x) 
-# a=60
-# print(bin(a))
+a=20
+b=10
+x=a+b
+print(x) 
+a=60
+print(bin(a))
  
-# a=345
-# print(oct(a))
-# print(bin(0o531))
-# a=234
-# print(hex(a))
-# a=50+60j
-# print(type(a))
-# print(a.real)
-# print(a.imag)
-# a=True
-# b=False
-# print(a)
-# print(b)
-# a=50
-# b=60
-# result=a>b
-# print(result)
+a=345
+print(oct(a))
+print(bin(0o531))
+a=234
+print(hex(a))
+a=50+60j
+print(type(a))
+print(a.real)
+print(a.imag)
+a=True
+b=False
+print(a)
+print(b)
+a=50
+b=60
+result=a>b
+print(result)
 a=True
 print(type(a))
 a="ï am chandu"
